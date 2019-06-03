ad4d21cf881a70af0df95183073a5b4e24ac095c
Laura buns <lawr@lauragonzalez.cc>
2019-06-03 17:10:41 +0200

    woop

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const ks = require('sendkeys-js');
 const { listConnectedJoyCons } = require('switch-joy-con');
 
 const con = listConnectedJoyCons()[0].open();
+const { a, b, x, y, dpadUp, dpadDown, dpadLeft, dpadRight } = con.buttons;
+
 con.on('change', () => {
-	const { a, b, x, y, dpadUp, dpadDown, dpadLeft, dpadRight } = con.buttons;
 	if ([a, b, x, dpadUp, dpadDown, dpadRight].includes(true)) {
 		ks.send('space');
 	}
